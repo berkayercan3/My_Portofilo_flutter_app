52136c7c5d01a7f5dd657c2475c58f1d869799e6
Berkay Ercan <115825102+berkayercan3@users.noreply.github.com>
2025-02-13 18:01:41 +0300

    Create Home Page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:my_portofilo/constants/color.dart';
 import 'package:my_portofilo/constants/nav_items.dart';
+import 'package:my_portofilo/styles/style.dart';
+import 'package:my_portofilo/widgets/header_desktop.dart';
+import 'package:my_portofilo/widgets/header_mobile.dart';
+import 'package:my_portofilo/widgets/site_logo.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,41 +21,10 @@ class _HomePageState extends State<HomePage> {
         body: (ListView(
           children: [
             //main
-            Container(
-              height: 60,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                  gradient: const LinearGradient(
-                      colors: [Colors.transparent, CustomColor.bglight1]),
-                  borderRadius: BorderRadius.circular(100)),
-              child: Row(
-                children: [
-                  const Text(
-                    "AK",
-                    style: TextStyle(
-                        fontSize: 22,
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.underline,
-                        color: CustomColor.yellowSecondary),
-                  ),
-                  Spacer(),
-                  for (int i = 0; i < navTitles.length; i++)
-                    Padding(
-                      padding: const EdgeInsets.only(right: 20),
-                      child: TextButton(
-                        onPressed: () {},
-                        child: Text(
-                          navTitles[i],
-                          style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: CustomColor.whitePrimary),
-                        ),
-                      ),
-                    )
-                ],
-              ),
+            //const HeaderDesktop(),
+            HeaderMobile(
+              onMenuTap: () {},
+              onLogoTap: () {},
             ),
             //skilss
             Container(
---
+++ lib/styles/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:my_portofilo/constants/color.dart';
+
+BoxDecoration kHederDecoration = BoxDecoration(
+    gradient: const LinearGradient(
+        colors: [Colors.transparent, CustomColor.bglight1]),
+    borderRadius: BorderRadius.circular(100));
